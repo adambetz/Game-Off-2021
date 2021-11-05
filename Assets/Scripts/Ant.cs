e237ac256ca98f004d07f1a33de6941e00cc577a
adambetz <adam.betzy@gmail.com>
2021-11-05 13:28:41 -0700

    Unity project Setup

---
+++ Assets/Scripts/Ant.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Ant : MonoBehaviour
+{
+    public float accuracy = 5.0f;
+
+    [SerializeField] private NavMeshAgent agent;
+    private Home home;
+
+    private Goal[] goals;
+    private GameObject targetGoal;
+
+    private GameObject currentTarget;
+
+    private void Awake()
+    {
+        home = FindObjectOfType<Home>();
+        goals = FindObjectsOfType<Goal>();
+    }
+
+    private void Start()
+    {
+        targetGoal = goals[Random.Range(0, goals.Length)].gameObject;
+        currentTarget = targetGoal;
+
+        Seek(currentTarget.transform.position);
+    }
+
+    private void Update()
+    {
+        Gather();
+    }
+
+    // Got to target position
+    public void Seek(Vector3 location)
+    {
+        agent.SetDestination(location);
+    }
+
+    public void Gather()
+    {
+        if (GetDistanceToTarget() <= accuracy)
+        {
+            SetNextTarget();
+            Seek(currentTarget.transform.position);
+        }
+    }
+
+    private float GetDistanceToTarget()
+    {
+        Vector3 direction = currentTarget.transform.position - this.transform.position;
+        return direction.magnitude;
+    }
+
+    private void SetNextTarget()
+    {
+        if(currentTarget == home.gameObject)
+        {
+            currentTarget = targetGoal;
+        }
+        else
+        {
+            currentTarget = home.gameObject;
+        }
+    }
+
+}
---
+++ Assets/Scripts/FlockManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockManager : MonoBehaviour
+{
+    public GameObject antPrefab = null;
+    public int numerOfAnts = 20;
+    public GameObject[] antsArray;
+    public Vector3 swimLimits = new Vector3(5,5,5);
+
+    private void Start()
+    {
+        antsArray = new GameObject[numerOfAnts];
+        for(int i=0; i<numerOfAnts; i++)
+        {
+            Vector3 pos = this.transform.position + new Vector3(Random.Range(-swimLimits.x, swimLimits.x),
+                                                                Random.Range(-swimLimits.y, swimLimits.y),
+                                                                Random.Range(-swimLimits.z, swimLimits.z));
+            antsArray[i] = (GameObject) Instantiate(antPrefab, pos, Quaternion.identity);
+        }
+    }
+}
