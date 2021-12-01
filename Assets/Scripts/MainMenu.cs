fec80280cf2f4647e15b72b899dd3a7e1067f977
IChoMo <92058713+IChoMo@users.noreply.github.com>
2021-11-30 23:48:21 -0800

    How to Play Menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,12 +4,27 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public AudioSource ButtonSound;
+    public GameObject HowToPlayMenu;
 
     public void StartGame()
     {
         ButtonSound.pitch = Random.Range(1f, 2f);
         ButtonSound.Play();
         SceneManager.LoadScene(1);
+    }
+
+    public void HowToPlay()
+    {
+        ButtonSound.pitch = Random.Range(1f, 2f);
+        ButtonSound.Play();
+        HowToPlayMenu.SetActive(true);
+    }
+
+    public void BackButton()
+    {
+        ButtonSound.pitch = Random.Range(1f, 2f);
+        ButtonSound.Play();
+        HowToPlayMenu.SetActive(false);
     }
 
     public void QuitGame()
